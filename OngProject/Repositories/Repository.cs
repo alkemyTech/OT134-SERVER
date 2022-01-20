e6531c153d7a0e3509f688b0fec548c1ef827674
Matías López <matilpcsd227@gmail.com>
2022-01-20 15:14:51 -0300

    Se añadio el Repositorio generico y se agrego el patron UnitOfWork

--- OngProject/Repositories/Repository.cs
+++ OngProject/Repositories/Repository.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using OngProject.DataAccess;
+using OngProject.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace OngProject.Repositories
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected DbContext RepositoryContext { get; set; }
+        public Repository(DbContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+        public IQueryable<T> FindAll()
+        {
+            return this.RepositoryContext.Set<T>().AsNoTracking();
+        }
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.RepositoryContext.Set<T>().Where(expression).AsNoTracking();
+        }
+        public void Create(T entity)
+        {
+            this.RepositoryContext.Set<T>().Add(entity);
+        }
+        public void Update(T entity)
+        {
+            this.RepositoryContext.Set<T>().Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            this.RepositoryContext.Set<T>().Remove(entity);
+        }
+        public void SaveChanges()
+        {
+            this.RepositoryContext.SaveChanges();
+        }
     }
 }
