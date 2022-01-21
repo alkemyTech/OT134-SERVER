0d59031fb0048cc95162dac8f4426bc2c8c3c9e9
Matías López <matilpcsd227@gmail.com>
2022-01-21 17:31:46 -0300

    Se creo el controlador y el servicio de News, ademas se añade en el UoW el repositorio para la entidad News

---
+++ OngProject/Core/Interfaces/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface INewsService
+    {
+        public IEnumerable<New> GetAll();
+        public New GetById();
+        public void Insert(New activities);
+        public void Update(New activities);
+        public void Delete(New activities);
+    }
+}
