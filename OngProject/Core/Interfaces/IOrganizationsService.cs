4876c0b38aaf4d39d6cc57bd6bc653c08dd8ca5f
Javier Paez <javierignacio.paez@gmail.com>
2022-01-23 14:03:37 -0300

    Organization Service interface added

---
+++ OngProject/Core/Interfaces/IOrganizationsService.cs
@@ -0,0 +1,14 @@
+﻿using OngProject.Entities;
+using System.Collections.Generic;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IOrganizationsService
+    {
+        public IEnumerable<Organizacion> GetAll();
+        public Activities GetById();
+        public void Insert(Organizacion organization);
+        public void Update(Organizacion organization);
+        public void Delete(Organizacion organization);
+    }
+}
