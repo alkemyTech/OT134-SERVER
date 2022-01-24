59d3d840fc90d824d2360534745708f80bd46345
JoseLuisFernandezGit <fernandezjooseluis@gmail.com>
2022-01-24 16:23:58 -0300

    repository, controller and service has been implemented for User

--- OngProject/Controllers/UserController.cs
+++ OngProject/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,42 @@ namespace OngProject.Controllers
     [Authorize]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
