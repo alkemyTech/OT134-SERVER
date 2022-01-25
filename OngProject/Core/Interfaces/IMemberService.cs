ce3738ec4ceeaef3d9ae53316c7a59565de3b804
Luciana <lucianapiomonardez@gmail.com>
2022-01-25 00:23:53 -0300

    Se crea el repositorio, servicio con interfaz y controller de member

---
+++ OngProject/Core/Interfaces/IMemberService.cs
@@ -0,0 +1,17 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IMemberService
+    {
+        public IEnumerable<Member> GetAll();
+        public Member GetById();
+        public void Insert(Member member);
+        public void Update(Member member);
+        public void Delete(Member member);
+    }
+}
