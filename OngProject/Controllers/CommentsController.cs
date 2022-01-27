dbff6f72e9ab994dd1fc4d7e7ece644ea695ae18
Javier Paez <javierignacio.paez@gmail.com>
2022-01-27 18:56:48 -0300

    Comments controller added

---
+++ OngProject/Controllers/CommentsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ICommentsService _commentsService;
+
+        public CommentsController(ICommentsService commentsService)
+        {
+            _commentsService = commentsService;
+        }
+
+        [HttpGet]
+        public async Task<Comment> Get()
+        {
+            return await _commentsService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
