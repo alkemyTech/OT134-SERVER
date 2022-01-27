cde141b019cf8c520c3a141446191c6c78e3d162
Javier Paez <javierignacio.paez@gmail.com>
2022-01-27 18:56:31 -0300

    Comments service and interface added

---
+++ OngProject/Core/Business/CommentsService.cs
@@ -0,0 +1,45 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Business
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CommentsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<Comment> GetAll()
+        {
+            return await _unitOfWork.CommentsRepository.FindAllAsync();
+        }
+
+        public Comment GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OngProject/Core/Interfaces/ICommentsService.cs
@@ -0,0 +1,15 @@
+﻿using OngProject.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface ICommentsService
+    {
+        Task<Comment> GetAll();
+        Comment GetById();
+        void Insert(Comment comment);
+        void Update(Comment comment);
+        void Delete(Comment comment);
+    }
+}
