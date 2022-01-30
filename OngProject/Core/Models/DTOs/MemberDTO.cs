6cf6110a6e499951060389db508c0a3306b3a1f3
Javier Paez <javierignacio.paez@gmail.com>
2022-01-30 11:33:59 -0300

    Members Controller: GetAll functionality added

---
+++ OngProject/Core/Models/DTOs/MemberDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class MemberDTO
+    {
+        [Required(ErrorMessage = "The Name Is Required")]
+        [StringLength(maximumLength: 255, ErrorMessage = "The Name Is Too Long")]
+        public string Name { get; set; }
+
+        [StringLength(maximumLength: 255, ErrorMessage = "The Description Is Too Long")]
+        public string Description { get; set; }
+    }
+}
