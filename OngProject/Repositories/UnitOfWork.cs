c5a4600ea687d19867c7e0ffdb253321318dabe7
Javier Paez <javierignacio.paez@gmail.com>
2022-01-31 10:37:16 -0300

    Comments repository added to UoW

--- OngProject/Repositories/UnitOfWork.cs
+++ OngProject/Repositories/UnitOfWork.cs
@@ -23,23 +23,25 @@ namespace OngProject.Repositories
         private readonly IRepository<New> _newsRepository;
         private readonly IRepository<Testimonials> _testimonialsRepository;
         private readonly IRepository<User> _userrepository;
-        private readonly IRepository<Member> _memberRepository;
+        private readonly IRepository<Member> _membersRepository;
         private readonly IRepository<Organization> _organizationRepository;
         private readonly IRepository<Category> _categoryRepository;
         private readonly IRepository<Rol> _rolRepository;
         private readonly IRepository<Slides> _slideRepository;
         private readonly IRepository<Contacts> _contactRepository;
+        private readonly IRepository<Comment> _commentsRepository;
         public IRepository<Activities> ActivitiesRepository => _activitiesRepository ?? new Repository<Activities>(_dbContext);
         public IRepository<New> NewsRepository => _newsRepository ?? new Repository<New>(_dbContext);
         public IRepository<Testimonials> TestimonialsRepository => _testimonialsRepository ?? new Repository<Testimonials>(_dbContext);
         public IRepository<User> UserRepository => _userrepository ?? new Repository<User>(_dbContext);
-        public IRepository<Member> MemberRepository => _memberRepository ?? new Repository<Member>(_dbContext);
+        public IRepository<Member> MembersRepository => _membersRepository ?? new Repository<Member>(_dbContext);
         public IRepository<Organization> OrganizationRepository => _organizationRepository ?? new Repository<Organization>(_dbContext);
         public IRepository<Category> CategoryRepository => _categoryRepository ?? new Repository<Category>(_dbContext);
         public IRepository<Rol> RolRepository => _rolRepository ?? new Repository<Rol>(_dbContext);
         public IRepository<Slides> SlideRepository => _slideRepository ?? new Repository<Slides>(_dbContext);
-        public IRepository<Contacts> ContactRepository => _contactRepository ?? new Repository<Contacts>(_dbContext);   
-         #endregion
+        public IRepository<Contacts> ContactRepository => _contactRepository ?? new Repository<Contacts>(_dbContext);
+        public IRepository<Comment> CommentsRepository => _commentsRepository ?? new Repository<Comment>(_dbContext);
+        #endregion
 
         #region Methods
         public void Dispose()
