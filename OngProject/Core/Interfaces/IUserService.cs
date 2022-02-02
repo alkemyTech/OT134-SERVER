cb5786d3885cfdcb885f39cf44f8638ca2b7cf74
Javier Paez <javierignacio.paez@gmail.com>
2022-02-02 10:12:49 -0300

    Users GetAll method fixed

--- OngProject/Core/Interfaces/IUserService.cs
+++ OngProject/Core/Interfaces/IUserService.cs
@@ -2,17 +2,12 @@ using OngProject.Core.Models.DTOs;
 using OngProject.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using OngProject.Core.Models.DTOs;
-using OngProject.Entities;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
 
 namespace OngProject.Core.Interfaces
 {
     public interface IUserService
     {
-        public Task<UserDTO> GetAll();
+        public Task<IEnumerable<UserDTO>> GetAll();
         public User GetById();
         public Task<UserDetailDto> Insert(UserRegisterDto dto);
         public void Update(User user);
