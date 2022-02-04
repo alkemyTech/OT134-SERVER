ae0c0e720d79fe948ba0fa5a9fde5071fe549f0b
alanlo97 <ahlan9673@gmail.com>
2022-02-04 19:16:42 -0300

    Endpoing GET Slide by Id Created

--- OngProject/Core/Business/SlideService.cs
+++ OngProject/Core/Business/SlideService.cs
@@ -1,8 +1,10 @@
 ﻿using OngProject.Core.Interfaces;
 using OngProject.Core.Mapper;
 using OngProject.Core.Models.DTOs;
+using OngProject.Core.Models.Response;
 using OngProject.Entities;
 using OngProject.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,7 +29,7 @@ namespace OngProject.Core.Business
 
         public async Task<ICollection<SlideDTO>> GetAll()
         {
-            var response = await _unitOfWork.SlideRepository.FindAllAsync();
+            var response = await _unitOfWork.SlideRepository.FindByConditionAsync(x => x.SoftDelete == false);
             if (response.Count == 0)
             {
                 return null;
@@ -43,9 +45,26 @@ namespace OngProject.Core.Business
             }
         }
 
-        public Slides GetById()
+        public async Task<Result> GetById(int id)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                Slides slide = await _unitOfWork.SlideRepository.GetByIdAsync(id);
+
+                if (slide != null && !slide.SoftDelete)
+                    return Result<Slides>.SuccessResult(slide);
+                else
+                {
+                    if (slide == null)
+                        return Result.FailureResult("No hay ningun Slide cargado con Id ingresado");
+                    else
+                        return Result.FailureResult("Slide con Id ingresado a sido eliminado previamente");
+                }
+            }
+            catch(Exception ex)
+            {
+                return Result.FailureResult(ex.Message);
+            }
         }
 
         public void Insert(Slides slides)
