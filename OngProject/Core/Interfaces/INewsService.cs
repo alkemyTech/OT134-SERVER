e2bd48a04f82faef4cdc12fad19fb3224b836a8d
JoseLuisFernandezGit <fernandezjooseluis@gmail.com>
2022-02-07 17:49:57 -0300

    minor changes

--- OngProject/Core/Interfaces/INewsService.cs
+++ OngProject/Core/Interfaces/INewsService.cs
@@ -1,17 +1,14 @@
 ﻿using OngProject.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OngProject.Core.Interfaces
 {
     public interface INewsService
     {
-        public IEnumerable<New> GetAll();
-        public New GetById();
-        public void Insert(New news);
-        public void Update(New news);
-        public void Delete(New news);
+        IEnumerable<New> GetAll();
+        New GetById();
+        void Insert(New news);
+        void Update(New news);
+        void Delete(New news);
     }
 }
