c42396921ff8184699bfe29d31bec861c7073f5b
JoseLuisFernandezGit <fernandezjooseluis@gmail.com>
2022-02-14 13:24:04 -0300

    Pagination has been added to getall of news according to OT134-96

--- OngProject/Core/Interfaces/INewsService.cs
+++ OngProject/Core/Interfaces/INewsService.cs
@@ -1,14 +1,13 @@
 ﻿using OngProject.Core.Models.DTOs;
+using OngProject.Core.Models.PagedResourceParameters;
 using OngProject.Core.Models.Response;
-using OngProject.Entities;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OngProject.Core.Interfaces
 {
     public interface INewsService
     {
-        Task<ICollection<NewDtoForDisplay>> GetAll();
+        Task<Result> GetAll(PaginationParams paginationParams);
         Task<Result> GetById(int id);
         Task<Result> Insert(NewDtoForUpload newDTO);
         Task<Result> Update(int id, NewDtoForUpload newsDTO);
